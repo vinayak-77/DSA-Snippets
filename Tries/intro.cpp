26b4e2bd3d4d7af8b97cc992e027e4f8d89cd916
vinayak-77 <vinayak270603@gmail.com>
2023-07-13 16:51:34 +0530

    Tries added

---
+++ Tries/intro.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+struct Node {
+	Node* links[26];
+	bool flag = false;
+	bool containsKey(char ch){
+		return links[ch-'a'] !=NULL;
+	}
+
+	void put(char ch,Node* node){
+		links[ch-'a'] = node
+	}
+
+	Node* get(char ch){
+		return links[ch-'a'];
+	}
+	bool setEnd(){
+		flag = true
+	}
+	bool isEnd(){
+		return flag
+	}
+}
+
+class Trie{
+
+private:
+	Node* root
+
+public:
+	Trie() {
+		root = new Node();
+	}
+
+	void insert(string word){  // O(length of word)
+		Node* node = root;
+
+		for(int i = 0;i<word.length();i++){
+			if(!node->containsKey(word[i])){
+				node->put(word[i],new Node());
+			}
+			node = node->get(word[i]);
+		}
+		node->setEnd();
+	}
+
+	bool search(string word){ // O(length of word)
+		Node* node = root;
+		for(int i = 0;i<word.length();i++){
+			if(!node->containsKey(word[i])){
+				return false;
+			}
+			node = node->get(word[i]);
+		}
+		return node->isEnd();
+	}
+
+	bool startsWith(string word){ // O(length of word)
+		Node* node = root;
+		for(int i = 0;i<word.length();i++){
+			if(!node->containsKey(word[i])){
+				return false;
+			}
+			node = node->get(word[i]);
+		}
+		return true;
+	}
+
+	int countWordsEqualto(string& word){
+
+	}
+	int countWordsStartWith(string& word){
+
+	}
+	void erase(string& word){
+		
+	}
+
+};
+
+
+int main(){
+
+}
